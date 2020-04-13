6807e404df0230f8b0dc60c266d37498844dfcd2
btynybekov <btynybekovt@gmail.com>
2020-04-13 18:41:16 +0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/beka/project/models"
+)
+
+func main() {
+	models.InitDB("root@tcp(localhost:3306)/Salon")
+	templates := template.Must(template.ParseFiles("templates/main.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := templates.ExecuteTemplate(w, "main.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+	http.HandleFunc("/catalog", CatalogIndex)
+	http.HandleFunc("/master", MasterIndex)
+	http.HandleFunc("/vacation", VacationIndex)
+	http.ListenAndServe(":8080", nil)
+}
+func CatalogIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+	sum, err := models.AllCatalog(w, r)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	for _, su := range sum {
+		fmt.Fprintln(w, su.Name, su.Surname, su.Specialty, su.Style, su.Price, su.WorkTime)
+
+	}
+}
+func MasterIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+	ms, err := models.Masters(w, r)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	for _, m := range ms {
+		fmt.Fprintln(w, m.MasterID, m.Name, m.Surname, m.Phone, m.Email)
+	}
+}
+
+func VacationIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+	
+}
